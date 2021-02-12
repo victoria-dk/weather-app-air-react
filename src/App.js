52fb14daf28599ad47f12cca3c5d6dcd3c746867
Victoria <victoria-dk@hotmail.com>
2021-02-12 18:46:35 +0100

    Added Github repository URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <footer>
+        Coded by Victoria Nielsen 👩‍💻{" "}
+        <a
+          href="https://github.com/victoria-dk/weather-app-air-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
