464b49c4e3b1f6e565198a9d1422c06f84b6b621
Victoria <victoria-dk@hotmail.com>
2021-02-13 12:08:06 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <footer>
-        Coded by Victoria Nielsen 👩‍💻{" "}
-        <a
-          href="https://github.com/victoria-dk/weather-app-air-react"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Air App</h1>
+        <footer>
+          Coded by Victoria Nielsen 👩‍💻{" "}
+          <a
+            href="https://github.com/victoria-dk/weather-app-air-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
