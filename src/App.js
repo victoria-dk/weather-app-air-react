cd09b973c494efd8af39628b4a2e17530eb8d3d3
Victoria <victoria-dk@hotmail.com>
2021-02-15 10:34:39 +0100

    Created component Weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather Air App</h1>
+        <Weather />
         <footer>
           Coded by Victoria Nielsen 👩‍💻{" "}
           <a
