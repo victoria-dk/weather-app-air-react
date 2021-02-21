a2f5dd7d045cf435e69f425851d9a48c174d2dc1
Victoria <victoria-dk@hotmail.com>
2021-02-21 23:39:00 +0100

    Edited style

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,21 +1,26 @@
 import React from "react";
 import CurrentDate from "./CurrentDate";
 import DayTime from "./DayTime";
-import WeatherIcon from "./WeatherIcon.js";
+import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <CurrentDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
+      <div className="current-location">
+        <h1>{props.data.city}</h1>
+      </div>
+      <div className="current-date-time">
+        <ul>
+          <li>
+            <CurrentDate date={props.data.date} />
+          </li>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+      </div>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-4">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon
@@ -23,23 +28,25 @@ export default function WeatherInfo(props) {
                 description={props.data.description}
               />
             </div>
-
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
-            </div>
-            <div className="daytime">
-              <ul>
-                <li>
-                  Sunrise: <DayTime time={props.data.sunrise} />
-                </li>
-                <li>
-                  Sunset: <DayTime time={props.data.sunset} />
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-4">
+          <div className="clearfix current-temperature">
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
+          <div className="daytime">
+            <ul className="suntime">
+              <li>
+                Sunrise: <DayTime time={props.data.sunrise} />
+              </li>
+              <li>
+                Sunset:
+                <DayTime time={props.data.sunset} />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-4">
           <ul>
             <li>Wind: {Math.round(props.data.wind)} km/h</li>
             <li>Feels like: {Math.round(props.data.feels)} °C</li>
