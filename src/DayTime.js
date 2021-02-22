6299556694c25f787015f2e20c217a458cf2ad26
Victoria <victoria-dk@hotmail.com>
2021-02-22 11:25:42 +0100

    Edited timezone

--- src/DayTime.js
+++ src/DayTime.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 export default function DayTime(props) {
-  let time = new Date(props.time);
-
-  let hours = time.getHours();
+  let hours = props.time.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = time.getMinutes();
+  let minutes = props.time.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
