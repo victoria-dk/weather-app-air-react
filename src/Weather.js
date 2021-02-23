9de23b7c285cbdbd17b172e02886416eba99040b
Victoria <victoria-dk@hotmail.com>
2021-02-23 14:36:19 +0100

    Corrected API Url

--- src/Weather.js
+++ src/Weather.js
@@ -27,7 +27,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "87429fdbf593621029427a484995b880";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
